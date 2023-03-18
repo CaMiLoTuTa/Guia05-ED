f6d24268e958fba0446e32b40f4aa6a35c1ed6f2
Adrián Camilo Tuta <camilotuta2004@gmail.com>
2023-03-17 22:08:53 -0500

    Terminado curso

---
+++ src/Curso/Code/Estudiante.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Curso.Code;
+
+/**
+ *
+ * @author tutaa
+ */
+public class Estudiante {
+
+    private String nombres, apellidos, direccion, celular, programaAc;
+
+    public Estudiante() {
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setProgramaAc(String programaAc) {
+        this.programaAc = programaAc;
+    }
+
+    public String getProgramaAc() {
+        return programaAc;
+    }
+
+}
